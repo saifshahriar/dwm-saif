484bb0d0a818b46d20cb87ed606af687bb3c6952
Saif <saif@saif.org>
2022-10-02 15:34:34 +0000

    initial commit

--- config.def.h
+++ config.def.h
@@ -1,58 +1,88 @@
-/* See LICENSE file for copyright and license details. */
+#include "colors.h"
+#include "movestack.c"
+#include "selfrestart.c"
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+#define ALT_TAG_DECOR_PATCH 1
+#if ALT_TAG_DECOR_PATCH
+#define UNDERLINETAGS_PATCH 0                   /* Both Patches doesn't work togather */
+#endif
+
+/* Appearance */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 12;       /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
-static const char col_fg[]          = "#c0caf5";
-static const char col_bg[]          = "#1a1b26";
-static const char col_black[]       = "#15161e";
-static const char col_red[]         = "#f7768e";
-static const char col_green[]       = "#9ece6a";
-static const char col_yellow[]      = "#e0af68";
-static const char col_blue[]        = "#7aa2f7";
-static const char col_magenta[]     = "#bb9af7";
-static const char col_cyan[]        = "#7dcfff";
-static const char col_white[]       = "#a9b1d6";
-
-static const char *colors[][3]      = {
-	/*               fg         bg          border   */
-	[SchemeNorm] = { col_fg,    col_bg,     col_bg   },
-	[SchemeSel]  = { col_blue,    col_bg,   col_blue },
+static const char *colors[][3]      = {         // Support for Xresources file is there. So set it through the xresources file
+	/*               fg             bg            border   */
+	[SchemeNorm] = { foreground,    background,   background },
+	[SchemeSel]  = { color4,        background,   color4 },
 };
 
+/* First Window Center Patch */
+static const float fw_height =  0.7;      /* Window height */
+static const float fw_width  =  0.8;      /* Window width */
+static const float fw_offy   = -0.05;     /* Y offset */
+
+#if UNDERLINETAGS_PATCH
 /* Tags */
 static const char *tags[] = { 
-    "dev", 
-    "web", 
-    "sys" , 
-    "code",
+    "  ", 
+    "  ",
+    "  ",
+    "  ",
+    "  ",
+    "  ",
+    "  ",
+    "  ",
+    " 漣 "
+};
+
+/* Underline below the tags */
+static const unsigned int ulinepad	    = 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 3;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int          ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+#else
+static const char *tags[] = { 
+    "dev",
+    "www",
+    "sys",
     "doc",
+    "vbox",
     "chat",
-    "git", 
-    "vid", 
-    "pref" 
+    "mus",
+    "vid",
+    "gfx"
 };
 
+static const char *alttags[] = {
+    "[dev]",
+    "[www]",
+    "[sys]",
+    "[doc]",
+    "[vbox]",
+    "[chat]",
+    "[mus]",
+    "[vid]",
+    "[gfx]"
+};
+#endif
+
+/* Window Rules */
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   CenterThisWindow?      monitor */
-	{ "Alacritty",NULL,       NULL,       0,            0,           1,                     -1 },
-	{ "Gimp",     NULL,       NULL,       0,            1,           0,                     -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,                     -1 },
+	/* class        instance    title   tags mask   isfloating  CenterThisWindow?   monitor */
+	{ "Alacritty",  NULL,       NULL,   0,          0,          1,                  -1 },
+	{ "Gimp",       NULL,       NULL,   0,          1,          0,                  -1 },
+	{ "Firefox",    NULL,       NULL,   1 << 8,     0,          0,                  -1 },
 };
 
-/* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+/* Layout(s) */
+static const float mfact            = 0.5;  /* factor of master area size [0.05..0.95] */
+static const int   nmaster          = 1;    /* number of clients in master area */
+static const int   resizehints      = 1;    /* 1 means respect size hints in tiled resizals */
+static const int   lockfullscreen   = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -61,8 +91,9 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
-/* key definitions */
-#define MODKEY Mod4Mask
+/* Key Definitions */
+#define MODKEY  Mod4Mask
+#define META    Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -73,35 +104,37 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 /* Keybindings */
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          SHCMD("dmenu_run -p 'Run:' -fn 'JetBrainsMono Nerd Font:size=10' -h 30")  },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ Mod1Mask,                     XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	//{ MODKEY,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+    { MODKEY|ControlMask,           XK_m,           togglemaximize,      {0}  },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -111,6 +144,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
