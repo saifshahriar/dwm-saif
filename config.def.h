515f76f03e790c3d8c447e9a2f7336d78dd579dc
saif <saif@saif>
2022-10-03 08:04:33 +0000

    Added pertag patch

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 12;       /* 2 is the default spacing around the bar's font */
+static const int user_bh            = 0;       /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
 static const char *colors[][3]      = {         /* Looks for colors in xresources file, if not found colors.h is used */
 	/*               fg             bg            border   */
