e87c4fb51322048fcb90cec6c26c5dc0bc54b35c
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-10-26 00:57:06 +0600

    Updated installation script

--- install.sh
+++ install.sh
@@ -2,19 +2,43 @@
 
 [ $(command -v doas) ] && auth=doas || auth=sudo
 
+make_clean() {
+	make clean && $auth make clean install
+}
+
+install() {
+	$auth make clean install
+	$auth cp ./dwm.desktop /usr/share/xsessions/
+}
+
+uninstall() {
+	$auth make uninstall
+}
+
 case "$1" in
 	-h)
-		echo "-i    or    --install    to install   dwm in   the system"
-		echo "-r    or    --remove     to uninstall dwm from the system"
-
-		#shellcheck disable=SC2006
-		echo "-m    or    --make       to run `make` command"
-		
-		#shellcheck disable=SC2006
-		echo "-mi   or                 to run `make` command"
-	;;
-	-m) make clean
+		echo "-i    or    --install    to install    dwm in   the system"
+		echo "-r    or    --remove     to uninstall  dwm from the system"
+		echo "-m    or    --make       to run 'make' command"
+		echo "-mc                      to run 'make' command"
+		echo "-mi                      to run 'make' command"
 		;;
-	-i) $auth make clean install
+	-i)             "$(install)"
+		;;
+	--install)      "$(install)"
+		;;
+	-m)             make
+		;;
+	--make)         make
+		;;
+	-mc)            make clean
+		;;
+	-mi)            "$(make_clean)"
+		;;
+	--make-install) "$(make_clean)"
+		;;
+	-r)             "$(uninstall)"
+		;;
+	--remove)       "$(uninstall)"
 		;;
 esac
