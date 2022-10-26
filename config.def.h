487adc5f6fae902e5e855926d5184eef0527f976
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-10-26 10:22:38 +0600

    format space-tabs

--- config.def.h
+++ config.def.h
@@ -155,14 +155,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  incrgaps,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  incrogaps,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_minus,  incrogaps,      {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_g,	   togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_g,      togglegaps,     {0} },
 	/* Tags */
 	{ MODKEY,                       XK_Left,   viewtoleft,     {0} },
 	{ MODKEY,                       XK_Right,  viewtoright,    {0} },
 	{ MODKEY|ShiftMask,             XK_Left,   tagtoleft,      {0} },
 	{ MODKEY|ShiftMask,             XK_Right,  tagtoright,     {0} },
 	{ MODKEY,                       XK_Escape, toggleview,     {.ui = ~0 } },
-	{ MODKEY,                       XK_0,      view,     	   {.ui = ~0 } },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
