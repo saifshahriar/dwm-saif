c30a83cbfacd7b90f7fa0819a84a10ee2f75206f
Saif Shahriar <sh.saif@proton.me>
2022-11-26 21:22:22 +0600

    format

--- config.h
+++ config.h
@@ -3,39 +3,41 @@
 #include "psudogaplessgrid.c"
 
 /* Appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int maxborder = 1;        /* 1 means no border is shown when a window is maximized */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int alttagsdecor       = 1;        /* 0 means no alternate tags */
-static const int ulinetags          = 0;        /* 1 means underline under tags */
-static const int rect_indicator     = 0;        /* 1 means rectangular indicator for active tags */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
-static const int warp               = 1;        /* 0 means no cursor warp when switching between windows or monitors */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
-static char *colors[][3]            = {         /* Looks for colors in xresources file, if not found colors.h is used */
+static const unsigned int alttagsdecor   = 1;        /* 0 means no alternate tags */
+static const unsigned int borderpx       = 2;        /* border pixel of windows */
+static const unsigned int maxborder      = 1;        /* 1 means no border is shown when a window is maximized */
+static const unsigned int rect_indicator = 0;        /* 1 means rectangular indicator for active tags */
+static const unsigned int showbar        = 1;        /* 0 means no bar */
+static const unsigned int smartgaps      = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int snap           = 32;       /* snap pixel */
+static const unsigned int topbar         = 1;        /* 0 means bottom bar */
+static const unsigned int ulinetags      = 0;        /* 1 means underline under tags */
+static const unsigned int warp           = 1;        /* 0 means no cursor warp when switching between windows or monitors */
+static const int user_bh                 = 2;        /* 2 is the default spacing around the bar's font */
+static const char *fonts[]               = { "JetBrainsMono Nerd Font:size=10" };
+static char *colors[][3]                 = {         /* Looks for colors in xresources file, if not found colors.h is used */
 	/*                fg             bg             border   */
 	[SchemeNorm]  = { foreground,    background,    background },
 	[SchemeSel]   = { background,    accent,        border },
 	[SchemeTitle] = { accent,        background,    NULL }
 };
 
+/* Gaps */
+static const unsigned int gappih = 10;    /* horiz inner gap between windows */
+static const unsigned int gappiv = 10;    /* vert inner gap between windows */
+static const unsigned int gappoh = 10;    /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 10;    /* vert outer gap between windows and screen edge */
+
 /* First Window Center Patch */
-static const float fw_height =  0.7;      /* Window height */
-static const float fw_width  =  0.8;      /* Window width */
-static const float fw_offy   = -0.05;     /* Y offset */
+static const float fw_height =  0.7;   /* Window height */
+static const float fw_width  =  0.8;   /* Window width */
+static const float fw_offy   = -0.05;  /* Y offset */
 
 /* Underline below the tags */
 static const unsigned int ulinepad     = 5;    /* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke  = 3;    /* thickness / height of the underline, setting the value to 0 hides the underline completely */
 static const unsigned int ulinevoffset = 0;    /* how far above the bottom of the bar the line should appear */
-static const int          ulineall     = 0;    /* 1 to show underline on all tags, 0 for just the active ones */
+static const unsigned int ulineall     = 0;    /* 1 to show underline on all tags, 0 for just the active ones */
 
 /* Tags */
 /* static const char *tags[] = { */
@@ -185,7 +187,7 @@ static const Key keys[] = {
 	 * If no Xresources file is not found in either one of the places,
 	 * dwm uses default theme from the colors array.
 	 */
-	{ MODKEY,                       XK_F5,     xrdb_reload,    {.v = NULL } }, 
+	{ MODKEY,                       XK_F5,     xrdb_reload,    {.v = NULL } },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },           /* Reload DWM */
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },           /* Quit DWM */
 };
